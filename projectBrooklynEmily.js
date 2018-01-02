2afd172c9b17008c0546602529376205ba889ff3
EMBOT415 <embot415@gmail.com>
2018-01-01 19:17:59 -0500

    clean it up

--- projectBrooklynEmily.js
+++ projectBrooklynEmily.js
@@ -6,26 +6,26 @@ $(document).ready(function() {
 //**************************************************************************************************************
 
 
-// update src and srcset attributes
-  $(function(){
-    $('.zoom-wrapper img').each(function(){
+	// update src and srcset attributes
+	$(function(){
+		$('.zoom-wrapper img').each(function(){
 
-    	// set variables 
-    	var $src = $(this).attr('src'); 
-      var $set = $(this).attr('srcset');
-      var $newSrc = $src.replace(/_background/g, '_silo');
-      var $newSet = $set.replace(/_background/g, '_silo');
+			// set variables 
+			var $src = $(this).attr('src'); 
+			var $set = $(this).attr('srcset');
+			var $newSrc = $src.replace(/_background/g, '_silo');
+			var $newSet = $set.replace(/_background/g, '_silo');
 
-      // check to see if the img src has _background in it.
-      if ($src.indexOf("_background") >= 0) {
-	      // replace exisiting sources 
-		    $(this).prop('src', $newSrc);
-		    $(this).prop('srcset', $newSet);
-		  } else {
-		  	console.log("the image is already the background version")
-		  }
-    })
-  })
+			// check to see if the img src has _background in it.
+			if ($src.indexOf("_background") >= 0) {
+				// replace exisiting sources 
+				$(this).prop('src', $newSrc);
+				$(this).prop('srcset', $newSet);
+			} else {
+				console.log("the image is already the background version")
+			}
+		})
+	})
 
 
 
@@ -38,19 +38,18 @@ $(document).ready(function() {
 	// set the media query in a variable
 	var mediaQuery768 = window.matchMedia('(min-width: 768px)');
 
-	  // Add an event listener
-	  mediaQuery768.addListener(changeMargin);
+	// Add an event listener
+	mediaQuery768.addListener(changeMargin);
 
-	  // Function to change margin-top based on media query
-	  function changeMargin(mediaQuery768) {    
-	    if (mediaQuery768.matches) {
-	      $('.zoom-wrapper img').css({'margin-top' : '60px'});
-	    } else {
-	      $('.zoom-wrapper img').css({'margin-top' : '0px'});
-	    }
-	  }
-
+	// Function to change margin-top based on media query
+	function changeMargin(mediaQuery768) {    
+		if (mediaQuery768.matches) {
+			$('.zoom-wrapper img').css({'margin-top' : '60px'});
+		} else {
+			$('.zoom-wrapper img').css({'margin-top' : '0px'});
+		}
+	}
 	changeMargin(mediaQuery768);
 
 
-});
+}); // end document.ready
